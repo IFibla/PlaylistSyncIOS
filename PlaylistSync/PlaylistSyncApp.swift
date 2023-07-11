6b0e766e428ad0047b1076318a0fa651bab94ec5
IFibla <76115905+ifibla@users.noreply.github.com>
2023-07-11 22:02:45 +0200

    Initial Commit

---
+++ PlaylistSync/PlaylistSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistSyncApp.swift
+//  PlaylistSync
+//
+//  Created by Ignasi Fibla on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
